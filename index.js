5c0ec2d3c7a589891f7c721ea90ef7af94aabd1f
Arthur <Strevitz@github.com>
2020-06-19 19:45:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#ffe606",
+    "#ffa899",
+    "#fa8975",
+    "#fa7259",
+    "#fc5639",
+    "#3336ff"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
